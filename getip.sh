d755eba33333c0d0f49250426d039691fb529c10
w47son <54062322+W47son@users.noreply.github.com>
2022-08-07 14:25:24 +0200

    Added example to getip.sh

--- getip.sh
+++ getip.sh
@@ -1,6 +1,9 @@
-#Gets your PUBLIC IP Address with the Duckduckgo Api
+#Gets your PUBLIC IP Address and geoIP with the Duckduckgo Api
+
+# Your IP address is x.x.x.x in Madrid, Spain (28005) #
 
 #Add the function to your .zshrc and run it with getip
+
 
 function getip(){
 output="$(curl -s "https://api.duckduckgo.com/?q=ip&format=json&pretty=1" | jq .Answer | tr '><:' '\n' | tr -d '"' | awk 'NR==1; NR==4' | xargs)"
